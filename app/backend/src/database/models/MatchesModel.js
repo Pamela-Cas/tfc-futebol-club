50c845e189eb37ea725fb85e220849e5478e759b
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-10-21 18:30:20 -0300

    comentando tem e matches models

--- app/backend/src/database/models/MatchesModel.js
+++ app/backend/src/database/models/MatchesModel.js
@@ -1,54 +1,54 @@
-import { Model, INTEGER, BOOLEAN } from 'sequelize';
+// import { Model, INTEGER, BOOLEAN } from 'sequelize';
 
-import database from '.';
+// import database from '.';
 
-import Teams from './TeamsModel';
+// import Teams from './TeamModel';
 
-export default class Matches extends Model {
-  id: number;
-  homeTeam: number;
-  homeTeamGoals: number;
-  awayTeam: number;
-  awayTeamGoals: number;
-  inProgress: number;
-}
+// export default class Matches extends Model {
+//   id: number;
+//   homeTeam: number;
+//   homeTeamGoals: number;
+//   awayTeam: number;
+//   awayTeamGoals: number;
+//   inProgress: number;
+// }
 
-Matches.init({
-  id: {
-    type: INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-  },
-  homeTeam: {
-    type: INTEGER,
-    allowNull: false,
-  },
-  homeTeamGoals: {
-    type: INTEGER,
-    allowNull: false,
-  },
-  awayTeam: {
-    type: INTEGER,
-    allowNull: false,
-  },
-  awayTeamGoals: {
-    type: INTEGER,
-    allowNull: false,
-  },
-  inProgress: {
-    type: BOOLEAN,
-    allowNull: false,
-  },
-}, {
-  sequelize: database,
-  timestamps: false,
-  underscored: true,
-  modelName: 'matches',
-});
+// Matches.init({
+//   id: {
+//     type: INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//     allowNull: false,
+//   },
+//   homeTeam: {
+//     type: INTEGER,
+//     allowNull: false,
+//   },
+//   homeTeamGoals: {
+//     type: INTEGER,
+//     allowNull: false,
+//   },
+//   awayTeam: {
+//     type: INTEGER,
+//     allowNull: false,
+//   },
+//   awayTeamGoals: {
+//     type: INTEGER,
+//     allowNull: false,
+//   },
+//   inProgress: {
+//     type: BOOLEAN,
+//     allowNull: false,
+//   },
+// }, {
+//   sequelize: database,
+//   timestamps: false,
+//   underscored: true,
+//   modelName: 'matches',
+// });
 
-Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
-Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+// Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+// Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
 
-Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
-Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
+// Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
+// Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
--- app/backend/src/database/models/TeamModel.js
+++ app/backend/src/database/models/TeamModel.js
@@ -1,29 +1,28 @@
-import { INTEGER, STRING, Model } from 'sequelize';
-import database from '.';
+// import { INTEGER, STRING, Model } from 'sequelize';
+// import database from '.';
 
-export default class Team extends Model {
-  id!: number;
-  teamName: string;
-  teamHome:
-}
-
-Team.init(
-  {
-    id: {
-      type: INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    teamName: {
-      type: STRING,
-      allowNull: false,
-    },
-  },
-  { underscored: true,
-    sequelize: database,
-    tableName: 'teams',
-    modelName: 'Team',
-    timestamps: false,
-  },
-);
+// export default class Team extends Model {
+//   id!: number;
+//   teamName: string;
+//   teamHome: any;
+// }
+// Team.init(
+//   {
+//     id: {
+//       type: INTEGER,
+//       primaryKey: true,
+//       autoIncrement: true,
+//       allowNull: false,
+//     },
+//     teamName: {
+//       type: STRING,
+//       allowNull: false,
+//     },
+//   },
+//   { underscored: true,
+//     sequelize: database,
+//     tableName: 'teams',
+//     modelName: 'Team',
+//     timestamps: false,
+//   },
+// );
