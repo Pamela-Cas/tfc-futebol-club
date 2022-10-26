68f9d942dc9299b44687ae5ca93094f09578a9cb
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-10-26 17:48:34 -0300

    criando HTTP_EXCEPTION

---
+++ app/backend/src/utils/HTTP_EXCEPTION.ts
@@ -0,0 +1,16 @@
+class HTTP_EXCEPTION extends Error {
+  status: number;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+  }
+}
+
+export default HTTP_EXCEPTION;
+
+/*  Se o valor inserido pelo usuário não corresponder ao tipo esperado,
+uma exceção HttpException será criada e gerada com uma mensagem personalizada com base no tipo de erro.
+https://learn.microsoft.com/pt-br/dotnet/api/system.web.httpexception?view=netframework-4.8
+Mentoria 28.1 - turma xp
+*/
